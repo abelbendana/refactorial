7af115a26d2238cde1ccb33153634c1fa16d8abd
abelbendana <43134973+abelbendana@users.noreply.github.com>
2019-12-14 14:30:50 +0100

    Add files via upload

---
+++ background.js
@@ -0,0 +1,24 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {urlContains: 'app.factorialhr.com/attendance/clock-in'},
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if(changeInfo.status == 'complete'){
+    if(tab.url.indexOf("app.factorialhr.com/attendance/clock-in?doPost=1") != -1){
+      main(1, tab);
+    }
+  }
+})
---
+++ library.js
@@ -0,0 +1,160 @@
+let slot1;
+let slot2;
+let slot3;
+let slot4;
+let tabURL;
+let workMonth;
+let workYear;
+let factorialURL = "https://api.factorialhr.com"; 
+let scriptButton = document.getElementById("launchScript");
+
+if(scriptButton){
+    
+    scriptButton.onclick = function () {
+        main(0, null);
+    };
+}
+
+async function setUrl(tab){
+    if(tab){
+        tabURL = tab.url;
+    }else{
+        tabURL = await new Promise(resolve => {
+            chrome.tabs.getSelected(null, tab => resolve(tab.url));
+        });
+    }
+}
+
+function getSlots(mode) {
+    if(mode === 1){
+        let base = tabURL.split("?")[1].split("&");
+        slot1 = base[1].split("=")[1];
+        slot2 = base[2].split("=")[1];
+        slot3 = base[3].split("=")[1];
+        slot4 = base[4].split("=")[1];
+    }else{
+        slot1 = document.getElementsByTagName('input')[0].value;
+        slot2 = document.getElementsByTagName('input')[1].value;
+        slot3 = document.getElementsByTagName('input')[2].value;
+        slot4 = document.getElementsByTagName('input')[3].value;
+    }
+}
+
+function setMonth() {
+    workMonth = tabURL.split("clock-in/")[1].split("/")[1];
+}
+
+function setYear() {
+    workYear = tabURL.split("clock-in/")[1].split("/")[0];
+}
+
+async function getData(mode, tab){
+    await setUrl();
+    getSlots(mode);
+    setMonth();
+    setYear();
+}
+
+
+async function main(mode, tab){
+    try{
+        await getData(mode, tab);
+        let employee_id = await getEmployeeId();
+        let period_id = await getPeriodId(employee_id);
+        let days_to_fill = await getDaysToFill(employee_id);
+        await fillDays(period_id,days_to_fill);
+        alert('Worked hours added correctly. Refreshing.');
+        chrome.tabs.reload();
+    } catch(error) {
+        alert('error: ' + error);
+    }
+}
+
+async function getEmployeeId(){
+    try{
+        const response = await fetch(factorialURL + "/tasks", {
+            method: "GET"
+        });
+
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            return jsonResponse[0].employee_id;
+        }
+    }catch(error) {
+        throw "Nothing to report";
+    }
+}
+
+async function getPeriodId(employeeId){
+    try{
+        const response = await fetch(factorialURL + "/attendance/periods?year=" + workYear + "&month=" + workMonth + "&employee_id=" + employeeId, {
+            method: "GET"
+        })
+
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            return jsonResponse[0].id;
+        }
+    }catch(error) {
+        throw error;
+    }
+}
+
+async function getDaysToFill(employeeId){
+    try{
+        const response = await fetch(factorialURL + "/attendance/calendar?id=" + employeeId + "&year=" + workYear + "&month=" + workMonth, {
+            method: "GET"
+        });
+
+        if (response.ok) {
+            let arrayDates = [];
+            const jsonResponse = await response.json();
+            jsonResponse.forEach(element => {
+                if(element.is_laborable && !element.is_leave && element.day <= new Date().getDate()){
+                    arrayDates.push(element.day);
+                }
+            });
+            return arrayDates;
+        }
+    }catch(error){
+        throw error;
+    }
+}
+
+async function fillDays(periodId, days){
+    try{
+        if(slot1 && slot2) {
+            days.forEach(dayToFill => {
+                fillDay(periodId, dayToFill, slot1, slot2);
+            });
+        }
+
+        if(slot3 && slot4) {
+            days.forEach(dayToFill => {
+                fillDay(periodId, dayToFill, slot3, slot4);
+            });
+        }
+    }catch(error){
+        throw error;
+    }
+}
+
+async function fillDay(periodId, dayToFill, clockIn, clockOut){
+    try{
+        const response =  await fetch(factorialURL + "/attendance/shifts", {
+            body: JSON.stringify({
+                clock_in: clockIn,
+                clock_out: clockOut,
+                day: dayToFill,
+                period_id: periodId
+            }),
+            headers: {
+                "Accept": "application/json, text/plain, */*",
+                "Content-Type": "application/json;charset=UTF-8"
+            },
+            method: "POST"
+        });
+    }catch(error){
+        throw error;
+    }
+}
