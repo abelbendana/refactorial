772eda8c851f0bb368a201425b377ce5e8c97bb3
Efrain Villanueva <efrain.villanueva@aboutyou.com>
2023-03-26 17:10:14 +0200

    refactor(library): refactor library functions
    - Remove unnecessary global variables
    - improve readability of the code
    - Remove dead code

--- library.js
+++ library.js
@@ -1,243 +1,192 @@
-let slot1;
-let slot2;
-let slot3;
-let slot4;
-let slot5;
-let slot6;
-let tabURL;
-let workMonth;
-let workYear;
-let factorialURL = "https://api.factorialhr.com";
+const FACTORIAL_URL = "https://api.factorialhr.com";
 
-const setUrl = async (tab) => {
-  if (tab) {
-    tabURL = tab.url;
+const getUrlFromTab = async () => {
+  const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+  return tab.url;
+};
+
+const getSlots = () => {
+  const [firstShiftClockIn, firstShiftClockOut] = document.querySelectorAll("#firstShift input");
+  const [breakClockIn, breakClockOut] = document.querySelectorAll("#break input");
+  const [secondShiftClockIn, secondShiftClockOut] = document.querySelectorAll("#secondShift input");
+  return {
+    firstShiftClockIn: firstShiftClockIn?.value,
+    firstShiftClockOut: firstShiftClockOut?.value,
+    breakClockIn: breakClockIn?.value,
+    breakClockOut: breakClockOut?.value,
+    secondShiftClockIn: secondShiftClockIn?.value,
+    secondShiftClockOut: secondShiftClockOut?.value
+  }
+};
+
+const getMonthAndYear = (urlFromTab) => {
+  const [year, month] = urlFromTab.split("clock-in/")[1].split("/");
+  return {
+    month,
+    year
+  }
+};
+
+const getTabData = async () => {
+  const urlFromTab = await getUrlFromTab();
+  const monthAndYear = getMonthAndYear(urlFromTab);
+  const clockInAndOutInput = getSlots();
+
+  return {
+    clockInAndOutInput,
+    monthAndYear
+  }
+};
+
+const fetchAccessId = async () => {
+  const response = await fetch(`${FACTORIAL_URL}/accesses`);
+
+  if (response.ok) {
+    const jsonResponse = await response.json();
+
+    for (let index = 0; index < jsonResponse.length; index++) {
+      if (jsonResponse[index].current) {
+        return jsonResponse[index].id;
+      }
+    }
+
+    throw "Unable to find access id.";
   } else {
-    tab = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-    tabURL = tab[0].url;
+    throw response.statusText;
   }
 };
 
-const getSlots = (mode) => {
-  if (mode === 1) {
-    let base = tabURL.split("?")[1].split("&");
-    slot1 = base[1]?.split("=")[1];
-    slot2 = base[2]?.split("=")[1];
-    slot3 = base[3]?.split("=")[1];
-    slot4 = base[4]?.split("=")[1];
-    slot5 = base[5]?.split("=")[1];
-    slot6 = base[6]?.split("=")[1];
+const fetchEmployeeId = async (accessId) => {
+  const response = await fetch(`${FACTORIAL_URL}/employees`);
+
+  if (response.ok) {
+    const jsonResponse = await response.json();
+
+    for (let index = 0; index < jsonResponse.length; index++) {
+      if (jsonResponse[index].access_id === accessId) {
+        return jsonResponse[index].id;
+      }
+    }
+
+    throw "Unable to find user.";
   } else {
-    slot1 = document.getElementById("input1")?.value;
-    slot2 = document.getElementById("input2")?.value;
-    slot3 = document.getElementById("input3")?.value;
-    slot4 = document.getElementById("input4")?.value;
-    slot5 = document.getElementById("input5")?.value;
-    slot6 = document.getElementById("input6")?.value;
+    throw response.statusText;
   }
 };
 
-const setMonth = () => {
-  workMonth = tabURL.split("clock-in/")[1].split("/")[1];
+const fetchPeriodId = async (employeeId, monthAndYear) => {
+  const { year, month } = monthAndYear;
+
+  const response = await fetch(`${FACTORIAL_URL}/attendance/periods?year=${year}&month=${month}&employee_id=${employeeId}`);
+
+  if (response.ok) {
+    const jsonResponse = await response.json();
+    return jsonResponse[0].id;
+  } else {
+    throw response.statusText;
+  }
 };
 
-const setYear = () => {
-  workYear = tabURL.split("clock-in/")[1].split("/")[0];
-};
+const getDaysToFill = async (employeeId, monthAndYear) => {
+  const { year, month } = monthAndYear;
 
-const getData = async (mode, tab) => {
-  await setUrl();
-  getSlots(mode);
-  setMonth();
-  setYear();
-};
+  const response = await fetch(`${FACTORIAL_URL}/attendance/calendar?id=${employeeId}&year=${year}&month=${month}`);
 
-const getAccessId = async () => {
-  try {
-    const response = await fetch(factorialURL + "/accesses", {
-      method: "GET",
+  if (response.ok) {
+    const arrayDates = [];
+    const jsonResponse = await response.json();
+
+    jsonResponse.forEach((element) => {
+      if (
+        element.is_laborable &&
+        !element.is_leave &&
+        new Date(element.date) <= new Date()
+      ) {
+        arrayDates.push(element.day);
+      }
     });
-
-    if (response.ok) {
-      const jsonResponse = await response.json();
-
-
-      for (let index = 0; index < jsonResponse.length; index++) {
-        if (jsonResponse[index].current) {
-          return jsonResponse[index].id;
-        }
-      }
-
-      throw "Unable to find access id.";
-    }
-  } catch (error) {
-    throw "Unable to find access id";
+    return arrayDates;
+  } else {
+    throw response.statusText;
   }
 };
 
-const getEmployeeId = async (accessId) => {
-  try {
-    const response = await fetch(factorialURL + "/employees", {
-      method: "GET",
+const fillDays = async (periodId, days, clockInAndOutInput) => {
+  const {firstShiftClockIn, firstShiftClockOut, breakClockIn, breakClockOut, secondShiftClockIn, secondShiftClockOut} = clockInAndOutInput;
+
+  if (firstShiftClockIn && firstShiftClockOut) {
+    days.forEach((dayToFill) => {
+      fillDay(periodId, dayToFill, firstShiftClockIn, firstShiftClockOut);
     });
-
-    if (response.ok) {
-      const jsonResponse = await response.json();
-
-      for (let index = 0; index < jsonResponse.length; index++) {
-        if (jsonResponse[index].access_id === accessId) {
-          return jsonResponse[index].id;
-        }
-      }
-
-      throw "Unable to find user.";
-    }
-  } catch (error) {
-    throw "Unable to find user";
   }
-};
-
-const getPeriodId = async (employeeId) => {
-  try {
-    const response = await fetch(
-      factorialURL +
-        "/attendance/periods?year=" +
-        workYear +
-        "&month=" +
-        workMonth +
-        "&employee_id=" +
-        employeeId,
-      {
-        method: "GET",
-      }
-    );
-
-    if (response.ok) {
-      const jsonResponse = await response.json();
-      return jsonResponse[0].id;
-    }
-  } catch (error) {
-    throw error;
+  
+  if (breakClockIn && breakClockOut) {
+    days.forEach((dayToFill) => {
+      fillDay(periodId, dayToFill, breakClockIn, breakClockOut, false);
+    });
   }
-};
-
-const getDaysToFill = async (employeeId) => {
-  try {
-    const response = await fetch(
-      factorialURL +
-        "/attendance/calendar?id=" +
-        employeeId +
-        "&year=" +
-        workYear +
-        "&month=" +
-        workMonth,
-      {
-        method: "GET",
-      }
-    );
-
-    if (response.ok) {
-      let arrayDates = [];
-      const jsonResponse = await response.json();
-      console.log(jsonResponse)
-      jsonResponse.forEach((element) => {
-        if (
-          element.is_laborable &&
-          !element.is_leave &&
-          new Date(element.date) <= new Date()
-        ) {
-          arrayDates.push(element.day);
-        }
-      });
-      return arrayDates;
-    }
-  } catch (error) {
-    throw error;
-  }
-};
-
-const fillDays = async (periodId, days, employeeId) => {
-  try {
-    if (slot1 && slot2) {
-      days.forEach((dayToFill) => {
-        fillDay(periodId, dayToFill, slot1, slot2);
-      });
-    }
-
-    if (slot5 && slot6) {
-      days.forEach((dayToFill) => {
-        fillDay(periodId, dayToFill, slot5, slot6, false);
-      });
-    }
-
-    if (slot3 && slot4) {
-      days.forEach((dayToFill) => {
-        fillDay(periodId, dayToFill, slot3, slot4);
-      });
-    }
-  } catch (error) {
-    throw error;
+  
+  if (secondShiftClockIn && secondShiftClockOut) {
+    days.forEach((dayToFill) => {
+      fillDay(periodId, dayToFill, secondShiftClockIn, secondShiftClockOut);
+    });
   }
 };
 
 const fillDay = async (periodId, dayToFill, clockIn, clockOut, workable = true) => {
-  try {
-    const response = await fetch(factorialURL + "/attendance/shifts", {
-      body: JSON.stringify({
-        clock_in: clockIn,
-        clock_out: clockOut,
-        day: dayToFill,
-        period_id: periodId,
-        workable
-      }),
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      method: "POST",
-    });
-  } catch (error) {
-    throw error;
+  const response = await fetch(`${FACTORIAL_URL}/attendance/shifts`, {
+    body: JSON.stringify({
+      clock_in: clockIn,
+      clock_out: clockOut,
+      day: dayToFill,
+      period_id: periodId,
+      workable
+    }),
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    method: "POST",
+  });
+  
+  if (!response.ok) {
+    throw response.statusText;
   }
 };
 
-const main = async (mode, tab) => {
+const start = async () => {
   try {
-    await getData(mode, tab);
-    let access_id = await getAccessId();
-    let employee_id = await getEmployeeId(access_id);
-    let period_id = await getPeriodId(employee_id);
-    let days_to_fill = await getDaysToFill(employee_id);
-    await fillDays(period_id, days_to_fill, employee_id);
-    alert("Worked hours added correctly. Refreshing.");
+    const {clockInAndOutInput, monthAndYear} = await getTabData();
+    const access_id = await fetchAccessId();
+    const employee_id = await fetchEmployeeId(access_id);
+    const period_id = await fetchPeriodId(employee_id, monthAndYear);
+    const days_to_fill = await getDaysToFill(employee_id, monthAndYear);
+    await fillDays(period_id, days_to_fill, clockInAndOutInput);
 
     chrome.tabs.reload()
   } catch (error) {
-    alert("error: " + error);
+    alert("Error: " + error);
   }
 };
 
-const removeSecondRow = () => {
-  document.getElementById("secondRow").remove();
+const removeSecondShiftRow = () => {
+  document.getElementById("secondShift").remove();
 };
 
-const removePauseRow = () => {
-  document.getElementById("pauseRow").remove();
+const removeBreakRow = () => {
+  document.getElementById("break").remove();
 };
 
-const launchScript = () => {
-  main(0, null);
+const fillTimesheet = () => {
+  start();
 };
 
 if (typeof document !== 'undefined') {
   document.addEventListener("DOMContentLoaded", () => {
-    const result = document.querySelectorAll('#deleteButton')
-    result[0].addEventListener('click', removePauseRow)
-    result[1].addEventListener('click', removeSecondRow)
-
-    document
-      .getElementById("launchScript")
-      .addEventListener("click", launchScript);
+    const [breakDeleteBtn, secondShiftDeleteBtn] = document.querySelectorAll('#deleteButton');
+    breakDeleteBtn.addEventListener('click', removeBreakRow);
+    secondShiftDeleteBtn.addEventListener('click', removeSecondShiftRow);
+    document.getElementById("fillTimesheet").addEventListener("click", fillTimesheet);
   });
 }
 
